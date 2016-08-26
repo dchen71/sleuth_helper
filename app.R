5a69f9cd1d74b90d039ac8f35714ee0b0e509c33
dchen71 <dchen12812@gmail.com>
2016-08-26 15:36:29 -0700

    switched to side panel view

--- app.R
+++ app.R
@@ -20,27 +20,64 @@ server = (function(input, output, session) {
         
         # update the widget value
         updateDirectoryInput(session, 'directory', value = path)
+        
+        output$folders = renderText({
+          folders = list.dirs(readDirectoryInput(session, 'directory'), full.names = T, recursive = FALSE)
+          folders
+        })
+        
+        output$samples = renderTable({
+          folders = dir(readDirectoryInput(session, 'directory'))
+          return(data.frame(folders))
+        })
       }
     }
   )
   
 
+  
+  #use output of folders to find all the actual directories with kallisto results
+  #use dir() to find the name of all the sample names
+  #create conditional list(setup thing to create number of conditions to setup)
+  #create table to input manually the conditions
+  
+  #setup transcript analysis
+  #setup gene level analysis
+  
+  #choice of likelihood test or wald test
+  #return back sleuth analyzed data
 })
 
 # Define UI for application
 ui = (fluidPage(
-  fluidRow(
-    column(1),
-    column(
-      width = 10,
-      
-      # Application title
-      titlePanel("Directory Input Demo"),
-      directoryInput('directory', label = 'selected directory', value = '~')
+  titlePanel("Sleuth Helper"),
 
+  sidebarLayout(
+    sidebarPanel(
+      directoryInput('directory', label = 'Select directory', value = '~'),
+      helpText("Select the directory that contains the quantified reads from Kallisto")
     ),
-    column(1)
+    mainPanel(
+      fluidRow(
+        column(1),
+        column(
+          width = 10,
+          
+          # Application title
+          verbatimTextOutput("folders"),
+          tableOutput("samples")
+          
+          #Want to show up table to create dataframe for conditions and a submit so it can save whatever it is
+          #Want to choose transcript/gene level afterwards
+          #Want to choose likelihood/wald or both and submit to save it
+          #Probably options to get back object, or the whatever files it has
+        ),
+        column(1)
+      )
+    )
   )
+  
+
 ))
 
 #Load single file shiny app
